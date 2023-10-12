44cd1a30f6c627ab55ba9b27c82324d75a06c924
DaC00KIE <119905464+DaC00KIE@users.noreply.github.com>
2023-10-12 15:22:15 +0700

    Just working on the course card, view model should be gucchi

--- app/src/main/java/com/example/visprog_assignment5/ui/view/Soal2View.kt
+++ app/src/main/java/com/example/visprog_assignment5/ui/view/Soal2View.kt
@@ -8,10 +8,14 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
@@ -25,12 +29,15 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.core.text.isDigitsOnly
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.visprog_assignment5.R
+import com.example.visprog_assignment5.ui.model.CourseData
 import com.example.visprog_assignment5.ui.viewmodel.Soal2ViewModel
 
 val ColorSoal2Blue: Color = Color(0xFF495D91)
@@ -58,80 +65,124 @@ fun Soal2View(courseViewModel: Soal2ViewModel = viewModel()){
         mutableStateOf("")
     }
 
-    Column(
+    LazyColumn(
         modifier = Modifier
             .fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.spacedBy(8.dp)
     ) {
-
-        Column(
-            modifier = Modifier
-                .padding(top = 16.dp)
-                .fillMaxWidth(0.95f),
-            verticalArrangement = Arrangement.spacedBy(8.dp)
-        ) {
-            Text(text = "Courses", fontSize = 24.sp, fontWeight = FontWeight.SemiBold)
-            Text(text = "Total SKS: ${courseViewModel.totalSKS}")
-            Text(text = "IPK: ${courseViewModel.IPK}")
-        }
-
-        Column(
-            modifier = Modifier
-                .fillMaxWidth(0.95f),
-            verticalArrangement = Arrangement.spacedBy(8.dp)
-        ) {
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.spacedBy(
-                    alignment = Alignment.CenterHorizontally,
-                    space = 16.dp
-                )
+        item {
+            Column(
+                modifier = Modifier
+                    .padding(top = 16.dp)
+                    .fillMaxWidth(0.95f),
+                verticalArrangement = Arrangement.spacedBy(8.dp)
             ) {
-                TextField( //sks textfield
-                    value = sks,
-                    onValueChange = { if (it.isDigitsOnly()) sks = it },
-                    modifier = Modifier
-                        .border(2.dp, ColorSoal2Blue, RoundedCornerShape(7))
-                        .weight(0.4f)
-                    ,
-                    label = { Text(text = "SKS")},
-                    colors = textFieldColors
-                )
-                TextField( //score textfield
-                    value = score,
-                    onValueChange = { if (it.isDigitsOnly()) score = it },
-                    modifier = Modifier
-                        .border(2.dp, ColorSoal2Blue, RoundedCornerShape(7))
-                        .weight(0.4f)
-                    ,
-                    label = { Text(text = "Score")},
-                    colors = textFieldColors
-                )
+                Text(text = "Courses", fontSize = 24.sp, fontWeight = FontWeight.SemiBold)
+                Text(text = "Total SKS: ${courseViewModel.totalSKS}")
+                Text(text = "IPK: ${courseViewModel.IPK}")
             }
-            Row(
-                horizontalArrangement = Arrangement.SpaceBetween
+
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth(0.95f)
+                    .padding(top = 8.dp),
+                verticalArrangement = Arrangement.spacedBy(8.dp)
             ) {
-                TextField( //name textfield
-                    value = name,
-                    onValueChange = { name = it },
-                    modifier = Modifier
-                        .border(2.dp, ColorSoal2Blue, RoundedCornerShape(7))
-                        .clip(RoundedCornerShape(16.dp)),
-                    label = { Text(text = "Name")},
-                    colors = textFieldColors
-                )
-                Button(
-                    onClick = { },
-                    colors = ButtonDefaults.buttonColors(ColorSoal2Blue),
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.spacedBy(
+                        alignment = Alignment.CenterHorizontally,
+                        space = 16.dp
+                    )
                 ) {
-                    Text(text = "+", color = Color.White)
+                    TextField( //sks textfield
+                        value = sks,
+                        onValueChange = { if (it.isDigitsOnly()) sks = it },
+                        modifier = Modifier
+                            .border(2.dp, ColorSoal2Blue, RoundedCornerShape(7))
+                            .weight(0.4f)
+                        ,
+                        label = { Text(text = "SKS")},
+                        colors = textFieldColors
+                    )
+                    TextField( //score textfield
+                        value = score,
+                        onValueChange = { if (it.isDigitsOnly()) score = it },
+                        modifier = Modifier
+                            .border(2.dp, ColorSoal2Blue, RoundedCornerShape(7))
+                            .weight(0.4f)
+                        ,
+                        label = { Text(text = "Score")},
+                        colors = textFieldColors
+                    )
+                }
+                Row(
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    TextField( //name textfield
+                        value = name,
+                        onValueChange = { name = it },
+                        modifier = Modifier
+                            .border(2.dp, ColorSoal2Blue, RoundedCornerShape(7))
+                            .clip(RoundedCornerShape(16.dp)),
+                        label = { Text(text = "Name")},
+                        colors = textFieldColors
+                    )
+                    Button(
+                        onClick = { },
+                        colors = ButtonDefaults.buttonColors(ColorSoal2Blue),
+                        modifier = Modifier
+                            .weight(1f)
+                            .padding(start = 12.dp)
+                    ) {
+                        Text(
+                            text = "+",
+                            color = Color.White,
+                            fontSize = 28.sp,
+                            fontWeight = FontWeight.Normal
+                        )
+                    }
                 }
             }
         }
 
+        item{
+            CourseCard()
+        }
+
 
 //        The Thingy
+    }
+}
+
+@Composable
+fun CourseCard(
+//    course: CourseData
+){
+    Card(
+        modifier = Modifier
+            .fillMaxWidth(0.95f),
+        colors = CardDefaults.cardColors(ColorSoal1Gray)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(12.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Column(
+                modifier = Modifier.fillMaxWidth(0.8f)
+            ) {
+
+            }
+            Icon(
+                painter = painterResource(id = R.drawable.baseline_delete_24),
+                contentDescription = "Delete Button",
+                tint = Color.Red)
+        }
     }
 }
 
